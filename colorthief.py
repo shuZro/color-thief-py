33edcc53d0638ff5c0b171c5fd52782d55fae6e8
Shuprio <shuprio.shourov@gmail.com>
2024-08-10 14:38:02 -0400

    Update colorthief.py

--- colorthief.py
+++ colorthief.py
@@ -29,14 +29,14 @@ class cached_property(object):
 
 class ColorThief(object):
     """Color thief main class."""
-    def __init__(self, file):
+    def __init__(self, image):
         """Create one color thief for one image.
 
         :param file: A filename (string) or a file object. The file object
                      must implement `read()`, `seek()`, and `tell()` methods,
                      and be opened in binary mode.
         """
-        self.image = Image.open(file)
+        self.image = image
 
     def get_color(self, quality=10):
         """Get the dominant color.
